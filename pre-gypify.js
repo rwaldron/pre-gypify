78b227024fc92164d2d734da013465bdb98af49e
Rick Waldron <waldron.rick@gmail.com>
2016-04-18 11:07:23 -0400

    Merge binary instead of overwriting
    Signed-off-by: Rick Waldron <waldron.rick@gmail.com>

--- pre-gypify.js
+++ pre-gypify.js
@@ -21,19 +21,25 @@ var packageJson = JSON.parse(fs.readFileSync("./package.json", "utf-8"));
 if (typeof packageJson.binary !== "undefined" &&
     hasDependency(packageJson, "node-pre-gyp")) {
 
+
+  // console.log(message);
+
   // The "binary" key is defined and the package.json includes
   // a dependency listing for node-pre-gyp, so it can be
   // assumed that the target module is providing its own
   // node-pre-gyp configuration and does not need the help
   // or interference of this tool.
-  process.exit(0);
+  // process.exit(0);
 }
 
 gyp("./binding.gyp", function (err, data) {
-  setImmediate(function () {
+  // setImmediate(function () {
     var targetName = data.targets.filter(function (target) {
       return !target.type;
     })[0].target_name;
+
+
+    console.log("pre-gypify, targetName: ", targetName);
 
     if (!data.targets.some(function (target) {
       return target.target_name == "action_after_build"
@@ -51,21 +57,27 @@ gyp("./binding.gyp", function (err, data) {
         ]
       });
       console.error("writing binding.gyp...");
+      console.log(JSON.stringify(data, null, '  '));
       fs.writeFileSync("./binding.gyp", JSON.stringify(data, null, '  '));
     }
 
+console.log("process.config.target_defaults.default_configuration?", process.config.target_defaults.default_configuration);
+
+
     if (!packageJson.binary) {
-      packageJson.binary = {
-        "module_name": targetName,
-        "module_path": `out/${process.config.target_defaults.default_configuration}/`,
-        "host": "example.com",
-      };
+      packageJson.binary = {};
     }
 
-    packageJson.binary.package_name = opts.package_name || "{module_name}-v{version}-{node_abi}-{platform}-{arch}.tar.gz";
+    Object.assign(packageJson.binary, {
+      module_name: targetName,
+      module_path: `out/${process.config.target_defaults.default_configuration}/`,
+      host: "example.com",
+    });
+
+    packageJson.binary.package_name = opts.package_name || "{module_name}-v{version}.tar.gz";
 
     console.error("writing package.json...");
     fs.writeFileSync("package.json", JSON.stringify(packageJson, null, "  "));
     console.error("kk");
-  });
+  // });
 });
